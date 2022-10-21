28bd693c2fdea1a41e776183e682dde271db2506
MicheleRai <rainonemichele1999@gmail.com>
2022-10-21 15:50:54 +0200

    js and html added

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function storeForm(){
+    const nome = document.getElementById("nome").value;
+    const km = document.getElementById('km').value;
+    const eta = document.getElementById('eta').value;
+    alert(nome + km + eta)
+    console.log(nome);
+    console.log(km);
+    console.log(eta);
+}
+
+//document.getElementById('nome-out').innerHTML = nome;
+//document.getElementById('km-out').innerHTML = km;
+//document.getElementById('eta-out').innerHTML = eta;
