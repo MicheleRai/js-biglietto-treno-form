0acc00fd4ba84d839a46fe9f243bc00f83fb8aec
MicheleRai <rainonemichele1999@gmail.com>
2022-10-23 17:35:13 +0200

    fix and improve

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ function storeForm(){
     console.log(eta);
     
     document.getElementById('nome-out').innerHTML = nome;
-    document.getElementById('km-out').innerHTML = km;
+    document.getElementById('km-out').innerHTML = km + 'km';
 
     var prezzo = km * 0.24;
     console.log('Prezzo senza sconti:' + prezzo);
@@ -18,19 +18,35 @@ function storeForm(){
         var prezzo = prezzo - (prezzo * 0.2 )
         var prezzo = prezzo.toFixed(2);
         document.getElementById('tipo').innerHTML = 'Biglietto ridotto per minore';
-        document.getElementById('prezzo').innerHTML = prezzo;
+        document.getElementById('prezzo').innerHTML = prezzo + '&#8364';
 
     }else if (age = 'over-65'){
         var prezzo = prezzo - (prezzo * 0.4 )
         var prezzo = prezzo.toFixed(2);
         document.getElementById('tipo').innerHTML = 'Biglietto ridotto per over 65';
-        document.getElementById('prezzo').innerHTML = prezzo;
+        document.getElementById('prezzo').innerHTML = prezzo + '&#8364';
 
         }else{
-            document.getElementById('prezzo').innerHTML = prezzo;
+            document.getElementById('prezzo').innerHTML = prezzo + '&#8364';
             document.getElementById('tipo').innerHTML = 'Biglietto standard';
         }
     
     console.log('Prezzo con sconti (se presenti):' + prezzo)
 }
 
+function generate() {
+    const  ticket = document.getElementById('ticket');
+    if (ticket.style.display === "none") {
+        ticket.style.display = "block";
+    } else {
+        ticket.style.display = "block";
+    }
+}
+function cancell() {
+    const  ticket = document.getElementById('ticket');
+    if (ticket.style.display === "block") {
+        ticket.style.display = "none";
+    } else {
+        ticket.style.display = "none";
+    }
+}
